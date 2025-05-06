26db061135c77a3b5e3e26efd7330dbc3a17293c
flmpl <flmp.leonardo@gmail.com>
2025-05-06 01:19:16 -0300

    feat(firebase): add conection firebase

---
+++ src/boot/firebase.ts
@@ -0,0 +1,31 @@
+import { defineBoot } from '#q-app/wrappers';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getDatabase } from 'firebase/database';
+import { getAuth } from 'firebase/auth';
+import { VueFire, VueFireAuth } from 'vuefire';
+
+const firebaseApp = initializeApp({
+  apiKey: 'AIzaSyDvaHnSU29r9zc29QkuMsG5A-s2dqPbsIg',
+  authDomain: 'jogo-da-memoria-278bd.firebaseapp.com',
+  projectId: 'jogo-da-memoria-278bd',
+  storageBucket: 'jogo-da-memoria-278bd.appspot.com',
+  messagingSenderId: '1063068944629',
+  appId: '1:1063068944629:web:005f10ea128e68d9245dbc',
+});
+
+const db = getFirestore(firebaseApp);
+const rtdb = getDatabase(firebaseApp);
+const auth = getAuth(firebaseApp);
+
+// Boot que instala o VueFire com VueFireAuth
+export default defineBoot(({ app }) => {
+  app.use(VueFire, {
+    firebaseApp,
+    modules: [
+      VueFireAuth()
+    ]
+  });
+});
+
+export { firebaseApp, db, rtdb, auth };
