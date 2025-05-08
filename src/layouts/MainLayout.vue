8f9194395375e3c45a30c2e9a7705e090d89baee
flmpl <flmp.leonardo@gmail.com>
2025-05-08 17:32:46 -0300

    feat(project): add dakmode

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,6 +5,7 @@ import { storeToRefs } from 'pinia';
 import { useRoute } from 'vue-router';
 import { computed, onMounted, onUnmounted, ref } from 'vue';
 import ModalSettings from 'components/organisms/ModalSettings.vue';
+import ModalTutorial from 'components/organisms/ModalTutorial.vue';
 
 const route = useRoute();
 const useUser = useUserStore();
@@ -13,6 +14,7 @@ const { getUser } = storeToRefs(useUser);
 const { playMusic, pauseMusic } = useAudio();
 
 const showModalSettings = ref(false);
+const showModalSchool = ref(false);
 
 const isHome = computed(() => route.fullPath === '/');
 
@@ -40,26 +42,21 @@ onUnmounted(() => {
 
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header class="bg-transparent text-cyan-9 q-py-md">
+    <q-header class="bg-transparent text-principal q-py-md">
       <div
         class="flex items-center mx-auto"
         :class="[isHome ? 'justify-end' : 'justify-between']"
         style="max-width: 900px"
       >
         <div v-if="!isHome" class="flex" style="gap: 10px">
-          <q-btn icon="arrow_back" round color="amber-7" to="/" />
+          <q-btn icon="arrow_back" round color="cyan-6" to="/" />
           <q-btn
             icon="settings"
             round
-            color="amber-7"
+            color="cyan-6"
             @click="showModalSettings = !showModalSettings"
           />
-          <q-btn
-            icon="school"
-            round
-            color="amber-7"
-            @click="showModalSettings = !showModalSettings"
-          />
+          <q-btn icon="school" round color="cyan-6" @click="showModalSchool = !showModalSchool" />
         </div>
         <div v-if="getUser" class="flex items-center self-end" style="gap: 10px">
           <q-img
@@ -70,15 +67,17 @@ onUnmounted(() => {
             class="q-btn--rounded"
           />
           <p class="no-margin">{{ getUser.nome }}</p>
-          <q-btn icon="logout" round color="amber-7" @click="useUser.logout" />
+          <q-btn icon="logout" round color="cyan-6" @click="useUser.logout" />
         </div>
       </div>
     </q-header>
 
-    <q-page-container class="bg-amber-2">
+    <q-page-container class="background-primary">
       <router-view />
     </q-page-container>
 
     <ModalSettings v-model="showModalSettings" />
+
+    <ModalTutorial v-model="showModalSchool" />
   </q-layout>
 </template>
